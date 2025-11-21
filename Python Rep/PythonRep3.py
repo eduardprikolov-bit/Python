a6a53c3c4c2eca4161866054fa1c9c3632cbcf91
By_lonely <eduardprikolov@gmail.com>
2025-11-21 13:22:59 +0300

    Create PythonRep3.py

---
+++ Python Rep/PythonRep3.py
@@ -0,0 +1,13 @@
+#Задание 1.
+#Количество минут от пользователя
+minutes = int(input(">>> "))
+
+#Часы и минуты
+hours = minutes // 60
+remaining_minutes = minutes % 60
+
+#Результат
+print(">>>", hours)
+print(">>>", remaining_minutes)
+
+#Задание 2.
