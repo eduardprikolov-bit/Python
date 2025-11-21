f373e9e319dd4b96849bc238b68ebae8cc4aadce
By_lonely <eduardprikolov@gmail.com>
2025-11-21 13:49:15 +0300

    Update PythonRep3.py

--- Python Rep/PythonRep3.py
+++ Python Rep/PythonRep3.py
@@ -11,3 +11,75 @@ print(">>>", hours)
 print(">>>", remaining_minutes)
 
 #Задание 2.
+a = int(input())  #минимальное рекомендуемое количество часов
+b = int(input())  #максимальное рекомендуемое количество часов
+h = int(input())  #сколько часов спит Аня
+
+#Проверка режима сна
+if h < a:
+    print("Недосып")
+elif h > b:
+    print("Пересып")
+else:
+    print("Это нормально")
+
+#Задание 3.
+#Ввод длин сторон треугольника
+a = float(input())
+b = float(input())
+c = float(input())
+
+#Вычисление полупериметра
+p = (a + b + c) / 2
+
+#Вычисление площади по формуле Герона
+area = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+
+#Вывод результата
+print(area)
+
+#Задание 4.
+#Ввод типа фигуры
+figure_type = input()
+
+if figure_type == "треугольник":
+    a = float(input())
+    b = float(input())
+    c = float(input())
+    #Формула Герона для треугольника
+    p = (a + b + c) / 2
+    area = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+    print(area)
+    
+elif figure_type == "прямоугольник":
+    a = float(input())
+    b = float(input())
+    #Площадь прямоугольника
+    area = a * b
+    print(area)
+    
+elif figure_type == "круг":
+    r = float(input())
+    #Площадь круга (п = 3.14)
+    area = 3.14 * r ** 2
+    print(area)
+
+#Задание 5.
+-
+#Задание 6.
+# Ввод номера билета
+ticket = input(">>> ")
+
+# Проверяем, что номер состоит из 6 цифр
+if len(ticket) == 6:
+    # Преобразуем каждую цифру в число и складываем
+    first_sum = int(ticket[0]) + int(ticket[1]) + int(ticket[2])
+    second_sum = int(ticket[3]) + int(ticket[4]) + int(ticket[5])
+    
+    # Сравниваем суммы
+    if first_sum == second_sum:
+        print(">>> Счастливый")
+    else:
+        print(">>> Обычный")
+else:
+    print(">>> Неверный номер билета")
